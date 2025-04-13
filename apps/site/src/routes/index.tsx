89213c4837a82d2c0f583a2227a11c14409e6470
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2025-04-13 14:31:14 +0545

    migrate to tailwind v4

--- apps/site/src/routes/index.tsx
+++ apps/site/src/routes/index.tsx
@@ -1,9 +1,7 @@
 import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { getCount, updateCount } from '@repo/api'
 
 export const Route = createFileRoute('/')({
   component: Home,
-  loader: () => getCount(),
 })
 
 function Home() {
@@ -16,12 +14,9 @@ function Home() {
         className='bg-red-300 text-3xl p-4 m-4'
         type="button"
         onClick={() => {
-          updateCount({ data: 1 }).then(() => {
-            router.invalidate()
-          })
         }}
       >
-        Add 1 to {state}?
+        Add 1 to 0?
       </button>
       <p>from cloudflare env ${import.meta.env.VITE_FROM_ENV}</p>
     </>
