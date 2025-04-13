d30262396c2d646e8524cc64ce8be06524ca3006
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2025-04-13 14:02:10 +0545

    update deps

--- apps/site/app.config.ts
+++ apps/site/app.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from '@tanstack/start/config'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from '@tanstack/react-start/config'
 import { cloudflare } from 'unenv'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  tsr: {
+    appDirectory: 'src',
+  },
   vite: {
     plugins: [
       viteTsConfigPaths({
         projects: ['./tsconfig.json'],
       }),
+      tailwindcss(),
     ],
   },
   server: {
