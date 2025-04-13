520dc15b0624dd6f0299159671d9212cb41a1c55
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2025-04-13 15:18:29 +0545

    add dummy hero section

--- apps/site/src/routes/index.tsx
+++ apps/site/src/routes/index.tsx
@@ -1,24 +1,14 @@
-import { createFileRoute, useRouter } from '@tanstack/react-router'
+import HeroSection from '@/components/hero-section'
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
-  const router = useRouter()
-  const state = Route.useLoaderData()
-
   return (
     <>
-      <button
-        className='bg-red-300 text-3xl p-4 m-4'
-        type="button"
-        onClick={() => {
-        }}
-      >
-        Add 1 to 0?
-      </button>
-      <p>from cloudflare env ${import.meta.env.VITE_FROM_ENV}</p>
+      <HeroSection />
     </>
   )
 }
