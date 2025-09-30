3528ed3010ce4e5bff4b852c264fcaa429beae44
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2025-09-30 17:50:16 +0545

    remove android dir from root

--- apps/site/src/integrations/tanstack-query/google-login-provider.tsx
+++ apps/site/src/integrations/tanstack-query/google-login-provider.tsx
@@ -14,7 +14,7 @@ export function GoogleLoginProvider({ children }: React.PropsWithChildren) {
 
 export function OneTapLoginProvider({ children }: React.PropsWithChildren) {
 	const { isAuthenticated } = useAuth();
-	if (!!isAuthenticated) return children;
+	if (isAuthenticated) return children;
 	return <_OneTapLoginProvider>{children}</_OneTapLoginProvider>;
 }
 
