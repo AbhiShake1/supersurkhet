40e229b9371c43a49af3e43ea5bfabf558e25125
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2025-11-21 18:55:32 +0545

    revert to radisk1

--- apps/relay/src/index.ts
+++ apps/relay/src/index.ts
@@ -1,6 +1,6 @@
 import Gun, { type IGunInstance } from 'gun';
 import { DurableObject } from "cloudflare:workers";
-import "gun/lib/radisk2"
+import "gun/lib/radisk"
 
 interface Env {
   GUN_PEERS: "";
