834bf68fdf0735520cd1a0dcc9d6833f4b296418
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2025-12-26 00:53:09 +0545

    Update app.config.ts

--- apps/site/app.config.ts
+++ apps/site/app.config.ts
@@ -2,8 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/react-start/config";
 import { cloudflare } from "unenv";
 import viteTsConfigPaths from "vite-tsconfig-paths";
-// import basicSsl from "@vitejs/plugin-basic-ssl";
-// import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   tsr: {
@@ -27,38 +25,27 @@ export default defineConfig({
       // reportCompressedSize: false,
       // TODO: set to true when gun+tanstackquery preloading is complete
       // ssr: false,
-      rollupOptions: {
-        onwarn(warning, defaultHandler) {
-          if (
-            warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
-            warning.message.includes('use client')
-          ) {
-            return
-          }
 
-          defaultHandler(warning)
-        },
-      },
+      // rollupOptions: {
+      //   onwarn(warning, defaultHandler) {
+      //     if (
+      //       warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
+      //       warning.message.includes('use client')
+      //     ) {
+      //       return
+      //     }
+      //
+      //     defaultHandler(warning)
+      //   },
+      // },
     },
     plugins: [
       {
-        name: 'strip-use-client',
-        enforce: 'pre',
-        transform(code, id) {
-          if (!id.includes('node_modules')) return
-
-          // remove any top-level "use client" directive
-          // even if comments or other directives exist before it
-          const stripped = code.replace(
-            /^[\s\S]*?(['"])use client\1;?\s*/m,
-            (match) => match.replace(/(['"])use client\1;?\s*/, '')
-          )
-
-          if (stripped !== code) {
-            return {
-              code: stripped,
-              map: null,
-            }
+        name: 'strip-use-client-directive',
+        // enforce: 'pre',
+        transform(code) {
+          if (code.startsWith("'use client'") || code.startsWith('"use client"')) {
+            return { code: `            ${code.slice(12)}`, map: null };
           }
         },
       },
