44c2c555acc05c93f254b8f14dfaf334eadfdc78
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2025-12-29 10:07:27 +0545

    WIP (retail/sale): add joining queries

--- apps/site/src/components/pages/retail/sales-page.tsx
+++ apps/site/src/components/pages/retail/sales-page.tsx
@@ -1,5 +1,9 @@
 import type { AdminComponent } from "@/components/ui/admin";
+import { api } from "@/lib/api";
 
 export const SalesPage: AdminComponent = ({ slug }) => {
+  const { data: products = [] } = api.product.useGet({
+    keys: [slug],
+  })
   return <div>Sales Page</div>;
 };
