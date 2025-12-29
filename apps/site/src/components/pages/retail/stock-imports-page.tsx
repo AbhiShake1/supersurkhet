85c34d0ab16fa6fe94b44c5bfbc4b5a71cf7aeb4
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2025-12-29 10:06:00 +0545

    WIP (retail/stock-imports): add joining queries

--- apps/site/src/components/pages/retail/stock-imports-page.tsx
+++ apps/site/src/components/pages/retail/stock-imports-page.tsx
@@ -1,5 +1,14 @@
 import type { AdminComponent } from "@/components/ui/admin";
+import { api } from "@/lib/api";
 
 export const StockImportsPage: AdminComponent = ({ slug }) => {
+  const { data: parties = [] } = api.party.useGet({
+    keys: [slug],
+  })
+
+  const { data: products = [] } = api.product.useGet({
+    keys: [slug],
+  })
+
   return <div>Stock Imports Page</div>;
 };
