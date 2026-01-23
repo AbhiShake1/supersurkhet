ee7e98233c68b825deb7117bc99dd197fe9e27de
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2026-01-23 14:17:57 +0545

    fix richtextfield

--- apps/site/src/components/ui/autoform/components/RichTextField.tsx
+++ apps/site/src/components/ui/autoform/components/RichTextField.tsx
@@ -1,33 +1,18 @@
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
-import type { FieldWrapperProps } from "./FieldWrapper";
+import type { AutoFormFieldProps } from "../react";
 
-export interface RichTextFieldProps extends FieldWrapperProps {
-  placeholder?: string;
-  className?: string;
-  rows?: number;
-}
-
-export function RichTextField({
-  field,
-  label,
+export const RichTextField: React.FC<AutoFormFieldProps> = ({
+  inputProps,
   error,
-  className,
-  placeholder,
-  rows = 4,
-  ...props
-}: RichTextFieldProps) {
+  id,
+}) => {
+  const { key, ...props } = inputProps;
+
   return (
-    <Textarea
-      id={field.name}
-      placeholder={placeholder}
-      className={cn(
-        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-        error && "border-destructive"
-      )}
-      rows={rows}
-      {...field}
-      {...props}
-    />
-  );
+    <Textarea id={id} className={cn(
+      "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+      error && "border-destructive",
+    )} {...props} />
+  )
 }
